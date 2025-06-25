15c07c28d3d7a54f9325c2a8d96f06f6d4597ccb
Luis Santiago Gonzalezrubio <lgsantiago07@gmail.com>
2025-06-25 16:02:57 -0500

    Rama  develop creada con la entidad y el application properties configurado   para postgresql

---
+++ src/main/java/user/microservice/pets/domain/model/User.java
@@ -0,0 +1,23 @@
+package user.microservice.pets.domain.model;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import user.microservice.pets.domain.enums.AuthProvider;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private UUID id;
+    private String username;
+    private String email;
+    private String password;
+    private LocalDateTime createdAt;
+    private AuthProvider authProvider;
+
+}
