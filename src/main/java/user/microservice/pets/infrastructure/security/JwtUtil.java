66931f7d0b9c5ad5af7d2ef7a05f3c07f4d9c2c2
Luis Santiago Gonzalezrubio <lgsantiago07@gmail.com>
2025-07-28 13:46:46 -0500

    Se modifico el sectret token paraque no se genere cada que sereinicie la app, si no tener uno secreto fijo

--- src/main/java/user/microservice/pets/infrastructure/security/JwtUtil.java
+++ src/main/java/user/microservice/pets/infrastructure/security/JwtUtil.java
@@ -2,8 +2,8 @@ package user.microservice.pets.infrastructure.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -13,8 +13,14 @@ import java.util.Map;
 @Component
 public class JwtUtil {
 
-    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    @Value("${jwt.secret}")
+    private String secret;
+
     private final long EXPIRATION = 1000 * 60 * 60; // 1 hora
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
 
     public String generateToken(String subject, Map<String, Object> claims) {
         return Jwts.builder()
@@ -22,16 +28,15 @@ public class JwtUtil {
                 .addClaims(claims)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
-                .signWith(key)
+                .signWith(getSigningKey())
                 .compact();
     }
 
     public Claims validateToken(String token) {
         return Jwts.parserBuilder()
-                .setSigningKey(key)
+                .setSigningKey(getSigningKey())
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
     }
-
 }
